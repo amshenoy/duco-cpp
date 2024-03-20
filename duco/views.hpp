1b726bde77284fe2f1a6ad82fce57ad5de2a5e59
Abhishek Shenoy <abhishekshenoy@hotmail.co.uk>
2024-03-20 21:21:52 +0000

    Create asset_day_view with std::views compatibility

--- duco/views.hpp
+++ duco/views.hpp
@@ -6,6 +6,7 @@
 #include <string>
 
 #include "data.hpp"
+#include "../utils/decompress_range.hpp"
 
 namespace duco::views
 {
@@ -30,5 +31,16 @@ namespace duco::views
             | std::views::transform(r2t(sym));
     }
 
+    auto asset_day_view(std::string symbol, year_month_day date) {
+        auto const filename = duco::data::File::getFilepath(symbol, date);
+        std::cout << filename << std::endl;
+        auto const sym = duco::data::RefData::get(symbol);
+        // This will work because the decompress_range is created at the point of method call
+        // return decompress_range_view(filename) | rows_view(sym);
+        // This returns an owning view which will be owned by the caller.
+        // return std::ranges::owning_view<decompress_range_view>(decompress_range_view(filename)) | rows_view(sym);
+        // This is a encapsulated version of the owning view of decompress_range_view
+        return decompress_range_owning_view(filename) | rows_view(sym);
+    }
 
 }
